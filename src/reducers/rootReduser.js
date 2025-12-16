6307dcce6c1b7e767b70c8534a2c6185d051dcb3
Alexey Dubnov <alex4972@mail.ru>
2025-12-16 19:10:21 +0200

    splitting

--- src/reducers/rootReduser.js
+++
@@ -1,9 +0,0 @@
-import {userReducer} from './userReducer.js';
-import {statsReduser} from './statsReduser.js';
-
-export const rootReduser = (state, action) => {
-  return{
-    user: userReducer(state.user, action),
-    stats: statsReduser(state.stats, action),
-  }
-}
